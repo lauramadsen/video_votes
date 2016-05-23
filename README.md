6d9c7ce37213dbb40fbd9fea768d8b6d32258d8c
Laura Madsen <laura@Fenrir.local>
2016-05-23 07:36:05 -0700

    init test mongo/node project

---
+++ README.md
@@ -0,0 +1,20 @@
+Up-to-date versions of Node (with npm) and MongoDB are required
+
+Lacking any of those?  On Mac, run:
+
+brew update
+brew install node
+brew install mongodb
+
+Get node dependencies:
+
+cd video_votes
+npm install
+
+Start your MongoDB:
+
+mongod --dbpath=/data --port 27017
+
+And run the application:
+
+npm start
---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+  insertDocuments(db, function() {
+    db.close();
+  });
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3030, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
